e3f011176f1c9faf68809136d5efc414de659e61
DenisZhi1 <159503111+DenisZhi1@users.noreply.github.com>
2025-08-26 13:24:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  //base: '/REPO_NAME/' // ← замени на имя репозитория, например '/my-landing/'
+  base: '/aiforkids/' // ← замени на имя репозитория, например '/my-landing/'
 })
