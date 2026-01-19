bb246c24e671da8ddbe0947912ca8430060bd7bb
DenisZhi1 <159503111+DenisZhi1@users.noreply.github.com>
2026-01-19 21:30:01 +0800

    Update app.js

--- public/Interactivepdf/app.js
+++ public/Interactivepdf/app.js
@@ -653,7 +653,7 @@ function placeEmojiRing(container, completed, emoji) {
 // === ССЫЛКИ НА ЗАДАНИЯ (вставьте сюда ваши URL)
 // Порядок: TASK_LINKS[0] — ссылка для задания 1, TASK_LINKS[1] — для задания 2 и т.д.
 const TASK_LINKS = [
-  "https://vk.ru/club235226792", // задача 1 — вставьте URL, например "https://site.example/task1"
+  "https://vk.ru/wall-235226792_12", // задача 1 — вставьте URL, например "https://site.example/task1"
   "https://vk.ru/club235226792", // задача 2
   "https://vk.ru/club235226792", // задача 3
   "https://vk.ru/club235226792", // задача 4
@@ -673,6 +673,7 @@ if (searchInput) {
 // Старт
 startDeadlineTimer();
 loadCSV();
+
 
 
 
